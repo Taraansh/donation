9aa8ea01f029d2ccbaced5d317f16a5226030d0d
chetan sharma <108821545+C-H-E-T-A-N@users.noreply.github.com>
2023-10-29 13:24:19 +0530

    cases error

--- src/page components/NgoProfileContainer.js
+++ src/page components/NgoProfileContainer.js
@@ -21,18 +21,21 @@ function NgoProfileContainer() {
     getAllNgos();
   }, [getAllNgos]);
 
+
   return (
     <div className="container" style={{ color: "white" }}>
       <div className="bg-color-1 my-3 p-5" style={{ borderRadius: "40px" }}>
         <h1 style={{ fontWeight: "bold" }}>Most Searched:</h1>
         <div className="row" style={{ textAlign: "center" }}>
           {allNgos.map((ngo, index) => (
-            <div className="col-sm my-3">
+            <div key={index} className="col-sm my-3">
               <div className="card profile-card" key={index}>
                 <img
-                  src={`http://127.0.0.1:8000${ngo.ngo_logo}`}
+                  // src={`http://127.0.0.1:8000${ngo.ngo_logo}`}
+                  src={logo}
                   className="card-img-profile"
                   alt="story-img"
+                  width='50%'
                   style={{ borderRadius: "10px", margin: "auto" }}
                 />
                 <div className="card-body">
@@ -75,42 +78,11 @@ function NgoProfileContainer() {
                     </Link>
                   </div>
                 </div>
-                <button className="btn profile-btn btn-primary">Donate</button>
+                <Link to={`/ngoprofile/${ngo.id}`} className="btn profile-btn btn-primary">VIEW</Link>
               </div>
             </div>
           ))}
 
-          <div className="col-sm my-3">
-            <div className="card profile-card">
-              <img
-                src={logo}
-                className="card-img-profile"
-                alt="story-img"
-                style={{ borderRadius: "10px", margin: "auto" }}
-              />
-              <div className="card-body">
-                <p className="card-text" style={{ fontWeight: "bold" }}>
-                  NGO_NAME
-                </p>
-                <div
-                  style={{
-                    padding: "0.5 rem",
-                    borderRadius: "7px",
-                    width: "fit-content",
-                    margin: "auto",
-                    boxShadow:
-                      "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",
-                  }}
-                >
-                  <i className="fa-brands mx-2 fa-facebook"></i>
-                  <i className="fa-brands mx-2 fa-twitter"></i>
-                  <i className="fa-brands mx-2 fa-instagram"></i>
-                  <i className="fa-brands mx-2 fa-threads"></i>
-                </div>
-              </div>
-              <button className="btn profile-btn btn-primary">Donate</button>
-            </div>
-          </div>
         </div>
       </div>
     </div>
